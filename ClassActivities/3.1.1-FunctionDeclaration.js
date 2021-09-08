643767c34cacaacd9d864cf3f63d6eeb766d66f8
Trishten Gregory <gtrishten63@gmail.com>
2021-09-07 19:24:19 -0500

    function declaration

---
+++ ClassActivities/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,15 @@
+function greet() {
+  console.log("hello");
+}
+function threeModTwo() {
+  console.log(3 % 2);
+}
+
+let age = 18;
+function checkDrivingAge() {
+  if (age >= 16) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
